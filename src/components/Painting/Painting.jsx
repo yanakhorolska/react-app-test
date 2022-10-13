dd8f789d3990d52a62c8886434f66c419056d99a
yanakhorolska <yana.khorolska@protonmail.com>
2022-10-13 22:31:31 +0200

    add icons

--- src/components/Painting/Painting.jsx
+++ src/components/Painting/Painting.jsx
@@ -1,6 +1,7 @@
 ﻿import PropTypes from 'prop-types';
 import defaultImage from './default-thumbnail.jpg';
-import css from './Painting.module.css'
+import css from './Painting.module.css';
+import { HiUser, HiCurrencyEuro, HiCheckCircle } from "react-icons/hi";
 
 export default function Painting({ img = defaultImage, title, authorUrl, authorName = "не відомо", price, quantity, type }) {
   
@@ -10,10 +11,10 @@ export default function Painting({ img = defaultImage, title, authorUrl, authorN
       <img className={css.img} src={img} alt={title} width="400" height="500" />
       <div className={css.info}>
         <h2 className= {css.title}>"{title}"</h2>
-      <p className={css.text}>Автор: <a href={authorUrl}>{authorName}</a></p>
-        <p className={css.text}>Ціна: <span className={css.price}>{price}</span></p>
+        <p className={css.text}><HiUser className={css.icon} size={25} /> Автор: <a href={authorUrl}>{authorName}</a></p>
+        <p className={css.text}><HiCurrencyEuro className={css.icon} size={25}/> Ціна: <span className={css.price}>{price}</span></p>
         
-          <p className={css.text}>Доступність: {quantity < 10 ? 'закінчується' : 'в наявності' }</p>
+          <p className={css.text}><HiCheckCircle className={css.icon} size={25}/> Доступність: {quantity < 10 ? 'закінчується' : 'в наявності' }</p>
       <button className={css.button} type="button">Додати в кошик</button>
       </div>
     </div>
